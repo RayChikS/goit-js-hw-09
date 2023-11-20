8836c233de1d678f369e2e450d4a2b3b1274d041
RayChikS <vatoresp@gmail.com>
2023-11-20 13:35:02 +0300

    first-task-complete

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const btnStart = document.querySelector('button[data-start]');
 
+const btnStop = document.querySelector('button[data-stop]');
+
+const body = document.querySelector('body');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+btnStart.addEventListener('click', () => {
+  btnStart.disabled = true;
+
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+btnStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  btnStart.disabled = false;
+});
---
+++ src/js/02-timer.js
@@ -0,0 +1,9 @@
+// Описаний в документації
+import flatpickr from 'flatpickr';
+// Додатковий імпорт стилів
+import 'flatpickr/dist/flatpickr.min.css';
+
+flatpickr('#datetime-picker', {
+  enableTime: true,
+  dateFormat: 'Y-m-d H:i',
+});
