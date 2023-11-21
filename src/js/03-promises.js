72fe8c59d620654d5d4aba4920714a299f2b85ed
RayChikS <vatoresp@gmail.com>
2023-11-21 11:03:06 +0300

    task-3-final

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,21 +1,41 @@
 import Notiflix from 'notiflix';
 const form = document.querySelector('.form');
-// const firstDelay = document.querySelector('input[name="delay"]');
-// const stepDelay = document.querySelector('input[name="step');
+const firstDelay = document.querySelector('input[name="delay"]');
+const stepDelay = document.querySelector('input[name="step');
 const amount = document.querySelector('input[name="amount');
-// const submit = document.querySelector('button');
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    Notiflix.Notify.failure(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  } else {
-    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      const result = { position, delay };
+
+      if (shouldResolve) {
+        resolve(result);
+      } else {
+        reject(result);
+      }
+    }, delay);
+  });
 }
 
-form.addEventListener('submit', () => {
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+
   for (let i = 0; i < amount.value; i++) {
-    createPromise();
+    const delay = parseInt(firstDelay.value) + i * parseInt(stepDelay.value);
+
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
   }
 });
