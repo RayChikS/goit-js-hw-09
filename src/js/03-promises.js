537a34bf40378a5f3884b930786272dbaba1eebf
RayChikS <vatoresp@gmail.com>
2023-11-21 09:52:03 +0300

    third-first-commit

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,21 @@
+import Notiflix from 'notiflix';
+const form = document.querySelector('.form');
+// const firstDelay = document.querySelector('input[name="delay"]');
+// const stepDelay = document.querySelector('input[name="step');
+const amount = document.querySelector('input[name="amount');
+// const submit = document.querySelector('button');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    Notiflix.Notify.failure(`✅ Fulfilled promise ${position} in ${delay}ms`);
   } else {
-    // Reject
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
   }
 }
+
+form.addEventListener('submit', () => {
+  for (let i = 0; i < amount.value; i++) {
+    createPromise();
+  }
+});
